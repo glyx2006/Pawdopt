229eb0b0c8fea7b02ba5335c370fc27fd36ec3b7
CarrotLP <lawpakcarrot@gmail.com>
2025-08-16 01:58:23 +0100

    adopter working, swiped right dogs will not show again

--- frontend/screens/shelters/EditShelterProfileScreen.tsx
+++ frontend/screens/shelters/EditShelterProfileScreen.tsx
@@ -26,7 +26,7 @@ import { NavigationProp, RouteProp, useNavigation, useRoute } from '@react-navig
 // Constants
 // ========================
 const API_PRESIGN_ICON_URL =
-  'https://1g53nof6f8.execute-api.eu-west-2.amazonaws.com/default/presignIconUrl';
+  'https://1g53nof6f8.execute-api.eu-west-2.amazonaws.com/presignIconUrl';
 
 const PUBLIC_DEFAULT_IMAGE =
   'https://static.vecteezy.com/system/resources/previews/009/292/244/non_2x/default-avatar-icon-of-social-media-user-vector.jpg';
